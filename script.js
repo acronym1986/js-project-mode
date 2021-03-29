40b9e691c6050d6a5e330f71337a13ae34024608
Tim <acronym1986@gmail.com>
2021-03-30 10:46:22 +1100

    css and js updated

--- script.js
+++ script.js
@@ -31,25 +31,31 @@ submitButton.onclick = searchWord;
           return data.json();
       })
       .then (function(data) {
-        console.log(JSON.stringify(data,null,2))   // if use JSON.stringify(data,null,2) to see array and object
+        console.log(data)   // if use JSON.stringify(data,null,2) to see array and object
         //word
         wordResult.innerText = data[0].meta.id;
         //definition
         defResult.innerText = data[0].shortdef;
+        
         //audio
+        let proAudio = data[0].hwi.prs[0].sound.audio;
+        let subdirectory = proAudio.charAt(0);
+        
+   let srcLink = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${subdirectory}/${proAudio}.mp3`;
+       
+        //set audio player
         let audio = document.createElement("AUDIO");
-        audio.setAttribute("src",data[0].hwi.prs[0].sound.audio);
-        audio.setAttribute("controls", "controls");
-        audio.setAttribute("autoplay", "autoplay");
-        pronounceResult.appendChild(audio);
+            audio.setAttribute("src",srcLink);
+            audio.setAttribute("controls", "controls");
+            audio.setAttribute("autoplay", "autoplay");
+            pronounceResult.appendChild(audio);
 
-
-      })
+      }) 
+       // define error response
       .catch (function(error) {
           console.log("error: " + error)
       }) 
-     
-
-   
+    
 }
 
+ 
